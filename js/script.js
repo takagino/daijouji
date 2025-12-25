30b283eab800eb31584ace5f20ba8224c4672826
takagino <hirotaka.takagi@trident.ac.jp>
2025-12-25 17:06:10 +0900

    1225

--- js/script.js
+++ js/script.js
@@ -1,42 +1,47 @@
 document.addEventListener('DOMContentLoaded', () => {
+  let resizeTimer;
+
   const hamburger = document.querySelector('.js-hamburger');
+  const gnav = document.querySelector('.gnav');
   const body = document.body;
 
   if (!hamburger) return;
 
-  hamburger.addEventListener('click', () => {
-    const expanded = hamburger.getAttribute('aria-expanded') === 'true';
+  const closeMenu = () => {
+    body.classList.remove('is-open');
+    hamburger.setAttribute('aria-expanded', 'false');
+    hamburger.setAttribute('aria-label', 'メニューを開く');
+  };
 
-    // 状態を反転
-    hamburger.setAttribute('aria-expanded', !expanded);
-    body.classList.toggle('is-open');
+  const toggleMenu = () => {
+    const isOpen = body.classList.contains('is-open');
+    if (isOpen) {
+      closeMenu();
+    } else {
+      body.classList.add('is-open');
+      hamburger.setAttribute('aria-expanded', 'true');
+      hamburger.setAttribute('aria-label', 'メニューを閉じる');
+    }
+  };
 
-    // スクリーンリーダー用のラベル切り替え
-    hamburger.setAttribute(
-      'aria-label',
-      expanded ? 'メニューを開く' : 'メニューを閉じる'
-    );
-  });
+  gnav.addEventListener('click', closeMenu);
+  hamburger.addEventListener('click', toggleMenu);
 
   // タブレットサイズ以上にリサイズされたらメニューを閉じる（念のための処理）
   window.addEventListener('resize', () => {
+    document.body.classList.add('is-resizing');
+
+    // タイマーをクリアして、リサイズが止まるまで待機
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+      // リサイズ停止から400ms後にクラスを削除
+      document.body.classList.remove('is-resizing');
+    }, 400);
+
     if (window.innerWidth >= 768) {
       // ブレイクポイントに合わせて調整
       body.classList.remove('is-open');
       hamburger.setAttribute('aria-expanded', 'false');
     }
   });
-});
-
-let resizeTimer;
-window.addEventListener('resize', () => {
-  // リサイズが始まったらクラスを付与
-  document.body.classList.add('is-resizing');
-
-  // タイマーをクリアして、リサイズが止まるまで待機
-  clearTimeout(resizeTimer);
-  resizeTimer = setTimeout(() => {
-    // リサイズ停止から400ms後にクラスを削除
-    document.body.classList.remove('is-resizing');
-  }, 400);
 });
